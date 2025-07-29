9b6961fe6386cf3658336b142491ebcca1152fc9
Natalie Chan <natkuma@outlook.com>
2025-07-28 23:51:17 -0400

    completed all checkpoints

--- ios101-project7-task/Task.swift
+++ ios101-project7-task/Task.swift
@@ -5,7 +5,7 @@
 import UIKit
 
 // The Task model
-struct Task {
+struct Task : Codable {
 
     // The task's title
     var title: String
@@ -53,24 +53,41 @@ struct Task {
 // MARK: - Task + UserDefaults
 extension Task {
 
-
+    private static let tasksKey = "savedTasks"
     // Given an array of tasks, encodes them to data and saves to UserDefaults.
     static func save(_ tasks: [Task]) {
-
-        // TODO: Save the array of tasks
+        let encoder = JSONEncoder()
+        do {
+            let data = try encoder.encode(tasks)
+            UserDefaults.standard.set(data, forKey: tasksKey)
+        } catch {
+            print("Error encoding tasks: \(error)")
+        }
     }
 
     // Retrieve an array of saved tasks from UserDefaults.
     static func getTasks() -> [Task] {
-        
-        // TODO: Get the array of saved tasks from UserDefaults
-
+        if let tasksData = UserDefaults.standard.data(forKey: tasksKey) {
+            let decoder = JSONDecoder()
+            do {
+                let tasks = try decoder.decode([Task].self, from: tasksData)
+                return tasks
+            } catch {
+                print("Error decoding saved tasks: \(error)")
+            }
+        }
         return [] // 👈 replace with returned saved tasks
     }
 
     // Add a new task or update an existing task with the current task.
     func save() {
-
-        // TODO: Save the current task
+        var tasks = Task.getTasks()
+        if let index = tasks.firstIndex(where: { $0.id == self.id }) {
+            tasks[index] = self
+        } else {
+            tasks.append(self)
+        }
+        Task.save(tasks)
     }
 }
+
